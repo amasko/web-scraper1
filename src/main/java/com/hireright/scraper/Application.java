24d0e36b6911a97982985a6edfbccab6df297904
amasko <alexey.masko@gmail.com>
2014-08-07 13:48:06 +0400

    page text content extracting

--- src/main/java/com/hireright/scraper/Application.java
+++ src/main/java/com/hireright/scraper/Application.java
@@ -4,7 +4,7 @@ public class Application {
 
     public static void main(String[] args) {
         PageProcessor pp = new PageProcessor();
-        pp.processPage("http://www.cnn.com");
+        pp.processPage("http://www.buzzfeed.com");
         System.out.println("done");
     }
 }
--- src/main/java/com/hireright/scraper/PageProcessor.java
+++ src/main/java/com/hireright/scraper/PageProcessor.java
@@ -3,10 +3,8 @@ package main.java.com.hireright.scraper;
 import java.io.*;
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.concurrent.TimeUnit;
 
-/**
- * Created by Alex on 06.08.2014.
- */
 public class PageProcessor {
     private File out;
 
@@ -26,16 +24,37 @@ public class PageProcessor {
         BufferedWriter writer = null;
         String result = "";
         try {
+            /*URLConnection con = url.openConnection();
+            InputStream is =con.getInputStream();*/
             URL url = new URL(urlStr);
             InputStreamReader inpReader = new InputStreamReader(url.openStream());
-            System.out.println(inpReader.getEncoding());
+            //System.out.println(inpReader.getEncoding());
             in = new BufferedReader(inpReader); //, "UTF-8"
             writer = new BufferedWriter(new OutputStreamWriter(new FileOutputStream(out, false)));//, "UTF-8"
             String str;
+            long t00 = System.nanoTime();
             while ((str = in.readLine()) != null) {
-                //result += str;
-                writer.write(str);
+                result += str;
+                //replacing markup from page and writing it to the output
             }
+            long t11 = System.nanoTime();
+            long s1 = TimeUnit.NANOSECONDS.toMillis(t11 - t00);
+            System.out.println("time scraping: " + s1);
+
+
+            // "\u00a0","" - non-breaking space
+            //
+            // (<script).*?(/script>) - regex for script block
+            // (<style).*?(/style>) - remove CSS
+            // &\S*?; - special HTML characters
+            // (<script).*?(/script>)|(<style).*?(/style>)
+
+            long t0 = System.nanoTime();
+            writer.write(result.replaceAll("(<script).*?(/script>)|(<style).*?(/style>)|<.*?>|&\\S*?;", "").replaceAll("\\s{2,}","&end;; "));//"\\<.*?\\>"
+            long t1 = System.nanoTime();
+            long s = TimeUnit.NANOSECONDS.toMillis(t1 - t0);
+            System.out.println("time removing: " + s);
+            //result.replace
             in.close();
             writer.close();
 
